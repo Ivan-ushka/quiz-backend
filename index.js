104c694faa4ba53ae1b0d29316f85c02df6f3886
Ivan Levchuk <ivanlev011287@gmail.com>
2024-12-21 14:49:00 +0300

    fix: change pathes

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const router = require('./router/index');
 const errorMiddleware = require('./middlewares/error-middleware');
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL
-}))
-app.use(cookieParser())
+}));
+app.use(cookieParser());
 app.use('/api', router);
 app.use(errorMiddleware);
 
-const start = async () => {
-    try {
-        app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-start()
-
-module.exports = {
-    app
-}
+module.exports = (req, res) => {
+    app(req, res);
+};
